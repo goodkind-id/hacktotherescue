2bdcff65cd2212683a790e18cdaf6870638feea8
Mickeel Pramono <mickeelps@gmail.com>
2023-12-06 16:12:49 +0700

    fetch json files

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { fetchLeaders } = require('./services/sync/leader')
+
+app.use(express.json())
+
+fetchLeaders()
+
 app.listen(port, () => {
   console.log(`Goodkind Hacktotherescue service listening on port ${port}`)
 })
